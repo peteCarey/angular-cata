09b2b115d96fb8a8d324f7ee712c7839eeb1debd
unknown <pete.carey@yahoo.co.uk>
2021-02-05 10:40:19 +0000

    constructor amended

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -9,6 +9,30 @@ import { Observable } from 'rxjs';
   styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent {
+  private baseUrl = 'https://jsonplaceholder.typicode.com/users';
+  users;
+  /*
+  constructor(private http: HttpClient) {}
+
+  getUser(id: number): Observable<any> {
+    console.log('test');
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createUser(user: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, user);
+  }
+*/
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get(this.baseUrl).subscribe((data) => {
+      console.log(data);
+      this.users = data;
+      console.log(this.users[0]);
+      console.log(this.users[1]);
+    });
+  }
+
   eMails = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
   model = new User(18, 'Dr IQ', this.eMails[0], 'Chuck Overstreet');
