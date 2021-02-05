29b8e2d58eb4b0739be3fffc68149a8dea2500c7
unknown <pete.carey@yahoo.co.uk>
2021-02-05 14:38:41 +0000

    user names displaying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   declarations: [AppComponent, UserFormComponent],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { UserService } from '../user.service';
 import { User } from '../user';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-form',
@@ -11,38 +8,14 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./user-form.component.css'],
 })
 export class UserFormComponent implements OnInit {
-  private baseUrl = 'https://jsonplaceholder.typicode.com/users';
-  //users = [];
-  // users: any;
-  /*
-  constructor(private http: HttpClient) {}
-
-  getUser(id: number): Observable<any> {
-    console.log('test');
-    return this.http.get(`${this.baseUrl}/${id}`);
-  }
-
-  createUser(user: Object): Observable<Object> {
-    return this.http.post(`${this.baseUrl}`, user);
-  }
-*/
-  users: any = [];
-  id: any;
-  constructor(public httpClient: HttpClient) {}
-
+  users;
+  constructor(private userservice: UserService) {}
   ngOnInit() {
-    this.httpClient
-      .get(this.baseUrl)
-      //  .pipe(map((data) => {}))
-      .subscribe((data) => {
-        this.users = data;
-        //console.log(data);
-        // this.users.push(data);
-        // console.log(this.users[0].id);
-        // this.id = this.users[0].id;
-        console.log(this.users);
-      });
+    this.users = this.userservice.getUsers();
+    console.info(this.users);
   }
+  //users = [];
+  // id: any;
 
   eMails = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
 
