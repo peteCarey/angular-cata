bc0446eb3f5f58a7f2aa4b87988fd7d5569ea4f5
unknown <pete.carey@yahoo.co.uk>
2021-02-08 11:40:18 +0000

    ceta users

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -17,11 +17,8 @@ export class UserFormComponent implements OnInit {
   ngOnInit() {
     this.lastIndex = 0;
     this.users = this.userservice.getUsers();
-    console.info(this.users);
     this.users.subscribe((res) => {
-      console.log(res);
       this.data = res;
-      console.log(this.data);
     });
   }
 
@@ -39,13 +36,14 @@ export class UserFormComponent implements OnInit {
     this.userId = 'id: ' + this.lastIndex;
     this.data[this.lastIndex - 1] = this.model; //adds the new user
     this.data[this.dataSize].id = this.lastIndex; // replace id 0 with last index
-
     console.log(this.data);
-    console.log(this.users);
-    console.log(typeof this.users);
   }
 
   newUser() {
     this.model = new User(this.lastIndex, '', '', '', '', '', '', '');
+  }
+
+  editUser() {
+    // To do
   }
 }
